b476bc18a0e2d10eedf418f8ca6e8863995358cb
xiuli.shen <xiuli.shen@openx.com>
2015-10-20 21:52:08 -0700

    Add microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
-  root 'application#hello'
+
+  root 'users#index'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021041317) do
+ActiveRecord::Schema.define(version: 20151021045006) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
